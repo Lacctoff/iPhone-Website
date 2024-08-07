566995460d5237e8693fd89c1eef9802c69a475f
Lacctoff <lasisitofunmi@gmail.com>
2024-08-07 01:44:02 +0100

    readjusted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -27,11 +27,11 @@ const Footer = () => {
 
             <div className="flex md:flex-row flex-col md:items-center justify-between">
                 <p className='font-semibold text-gray text-xs'>Copyright @ 2024 Apple Inc. All rights reserved</p>
-                <div className='flex'>
+                <div className='flex flex-1'>
                     {footerLinks.map((link, i) => (
                         <p key={link} className='font-semibold text-gray text-xs'>
                             {link}{' '}
-                            {i !== footerLinks.length - 1 && (<span className='mx-2 sm:mx-0'> | </span>)}
+                            {i !== footerLinks.length - 1 && (<span className='mx-2'> | </span>)}
                         </p>
                     ))}
                 </div>
